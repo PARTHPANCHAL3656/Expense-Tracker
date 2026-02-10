aa251a9ccd3789990d4e60b87161cd6875ea2b89
Parth Panchal <parthpanchal.10000@gmail.com>
2026-02-11 00:13:11 +0530

    may be service worker fix it ?

--- service-worker.js
+++ service-worker.js
@@ -4,15 +4,34 @@ const urlsToCache = [
   "/Expense-Tracker/home.html",
   "/Expense-Tracker/dashboard.html",
   "/Expense-Tracker/history.html",
+
   "/Expense-Tracker/styles/home.css",
   "/Expense-Tracker/styles/dashboard.css",
   "/Expense-Tracker/styles/history.css",
   "/Expense-Tracker/styles/nav.css",
   "/Expense-Tracker/styles/install.css",
+
   "/Expense-Tracker/scripts/home.js",
   "/Expense-Tracker/scripts/dashboard.js",
   "/Expense-Tracker/scripts/history.js",
   "/Expense-Tracker/scripts/install.js",
+
+  "/Expense-Tracker/icons/svg/add.svg",
+  "/Expense-Tracker/icons/svg/history.svg",
+  "/Expense-Tracker/icons/svg/dashboard.svg",
+
+  "/Expense-Tracker/icons/svg/calendar-days.svg",
+  "/Expense-Tracker/icons/svg/car.svg",
+  "/Expense-Tracker/icons/svg/clapperboard.svg",
+  "/Expense-Tracker/icons/svg/graduation-cap.svg",
+  "/Expense-Tracker/icons/svg/home.svg",
+  "/Expense-Tracker/icons/svg/indian-rupee.svg",
+  "/Expense-Tracker/icons/svg/pill.svg",
+  "/Expense-Tracker/icons/svg/shirt.svg",
+  "/Expense-Tracker/icons/svg/shopping-cart.svg",
+  "/Expense-Tracker/icons/svg/user-round-pen.svg",
+  "/Expense-Tracker/icons/svg/zap.svg",
+  
   "/Expense-Tracker/icon-192.png",
   "/Expense-Tracker/icon-512.png",
 ];
